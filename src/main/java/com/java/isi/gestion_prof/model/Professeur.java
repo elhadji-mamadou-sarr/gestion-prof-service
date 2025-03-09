a8ccc7c9a8993366f7e5bdbe8ac2f2a29c7ee6bf
elhadji-mamadou-sarr <ehm.sarr3@isepdiamniadio.edu.sn>
2025-03-09 07:51:15 +0000

    Configuration kub

--- src/main/java/com/java/isi/gestion_prof/model/Professeur.java
+++ src/main/java/com/java/isi/gestion_prof/model/Professeur.java
@@ -2,13 +2,13 @@ package com.java.isi.gestion_prof.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-
 @Setter
 @Getter
 @Entity
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Professeur {
-
     @Id
     @GeneratedValue
     private Long id;
@@ -20,22 +20,9 @@ public class Professeur {
     private String prenom;
 
     private String email;
-
     private String telephone;
 
     @ManyToOne
     @JoinColumn(name = "matiere_id")
     private Matiere matiere;
-
-    public Professeur() {
-
-    }
-
-    public Professeur(String nom, String prenom, String email, String telephone) {
-        this.nom = nom;
-        this.prenom = prenom;
-        this.email = email;
-        this.telephone = telephone;
-    }
-
 }
