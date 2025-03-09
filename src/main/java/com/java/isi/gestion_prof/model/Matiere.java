b19967d2e1e0acfa801babcba9f7b16a62c7f24c
elhadji-mamadou-sarr <ehm.sarr3@isepdiamniadio.edu.sn>
2025-03-09 08:16:57 +0000

    Configuration kub

--- src/main/java/com/java/isi/gestion_prof/model/Matiere.java
+++ src/main/java/com/java/isi/gestion_prof/model/Matiere.java
@@ -5,9 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
-import lombok.Builder;
+import lombok.*;
 
+@Setter
+@Getter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Matiere {
     @Id
@@ -20,31 +24,4 @@ public class Matiere {
     @Column(nullable = false, length = 100)
     private String designation;
 
-    public Matiere() {
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getLibelle() {
-        return libelle;
-    }
-
-    public void setLibelle(String libelle) {
-        this.libelle = libelle;
-    }
-
-    public String getDesignation() {
-        return designation;
-    }
-
-    public void setDesignation(String designation) {
-        this.designation = designation;
-    }
 }
--- src/main/java/com/java/isi/gestion_prof/model/Professeur.java
+++ src/main/java/com/java/isi/gestion_prof/model/Professeur.java
@@ -3,7 +3,11 @@ package com.java.isi.gestion_prof.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+@Setter
+@Getter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Professeur {
     @Id
@@ -23,10 +27,6 @@ public class Professeur {
     @JoinColumn(name = "matiere_id")
     private Matiere matiere;
 
-    public Professeur() {
-
-    }
-
     public Professeur(String nom, String prenom, String email, String telephone) {
         this.nom = nom;
         this.prenom = prenom;
@@ -34,51 +34,5 @@ public class Professeur {
         this.telephone = telephone;
     }
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public Matiere getMatiere() {
-        return matiere;
-    }
-
-    public void setMatiere(Matiere matiere) {
-        this.matiere = matiere;
-    }
 }
